33c4fdd4069057aa48c00243a678fdcceb9e24a7
Boris Fritscher <boris@fritscher.ch>
2021-01-10 08:23:23 +0100

    add sentry

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import i18n from "./plugins/i18n";
+import "./plugins/sentry";
 
 createApp(App)
   .use(store)
